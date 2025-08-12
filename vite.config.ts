8ea1c72d402f8911d38066e819b076451b316d1d
kiotobat <lambar926@gmail.com>
2025-08-12 16:44:23 +0300

    redit vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_props-catalog/',
+  base: '/propslist/',
   server: {
     open: true,
   },
